7099bf3409b72c3109c471cdf014c0d1090ee21f
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-08-01 23:58:21 +0200

    chore&build: update README and build gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: "Think1st",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
